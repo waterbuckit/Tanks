91d11291eadb54527b72f9e38a4f350c9490cad4
waterbuckit <adamroyal232@yahoo.com>
2018-03-02 22:15:35 +0000

    Main py file for handling all interaction with the game

---
+++ TanksMain.py
@@ -0,0 +1,25 @@
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+# Frame dimensions
+WIDTH = 1200
+HEIGHT = 800
+
+class Interaction:
+
+    def __init__(self):
+        pass
+
+    # Method for handling drawing all objects in the scene
+    def drawHandler(self, canvas):
+        pass
+    # Method for handling mouse clicks
+    def mouseClickHandler(self, position):
+        pass
+
+i = Interaction()
+
+# Frame initialisation
+frame = simplegui.create_frame('Tanks', WIDTH, HEIGHT)
+frame.set_mouseclick_handler(i.mouseClickHandler)
+frame.set_draw_handler(i.drawHandler)
+frame.start()
