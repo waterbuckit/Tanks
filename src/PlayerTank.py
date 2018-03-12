250f9fa38312f23af6998232b38e61e69c2ca5d2
waterbuckit <adamroyal232@yahoo.com>
2018-03-12 08:53:37 +0000

    Added some comments

--- src/PlayerTank.py
+++ src/PlayerTank.py
@@ -23,12 +23,13 @@ class PlayerTank(Tank):
         Tank.update(self, mousePos)
 
     def draw(self, canvas):
+        # draws a the line from the muzzle to the cursor position
         aimingLine = DottedLine(self.pos, Vector(self.mousePos[0], self.mousePos[1])).draw(canvas)
         canvas.draw_image(self.cursor, (self.cursor.get_width()/2, self.cursor.get_height()/2), 
                 (self.cursor.get_width(), self.cursor.get_height()), self.mousePos, (20, 20))
         self.drawReloadStatus(canvas, self.mousePos, 9)
         Tank.draw(self, canvas)
-    
+    # Draws the circle surrounding the cursor point indicating how "reloaded" the tank is
     def drawReloadStatus(self, canvas, mousePos, radius):
         angle = (self.reloadCounter/self.interval) * 360
         for i in range(int(angle)):
@@ -41,6 +42,7 @@ class PlayerTurret(Turret):
         Turret.__init__(self, base, pos)
 
     def update(self, mousePos):
+        # Handles the rotation of vectors for the vertices of the tank
         self.updateRotation(Vector(mousePos[0], mousePos[1]))
         gen = self.generator.copy()
         self.mesh = list()
