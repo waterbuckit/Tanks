eed88e56c7651914782a62eba2cab038ce3e33e5
2ammY <35489633+2ammY@users.noreply.github.com>
2018-03-15 10:40:17 +0000

    Add files via upload

--- Terrain.py
+++ Terrain.py
@@ -1,7 +1,7 @@
 try:
-    import simplegui
+	import simplegui
 except ImportError:
-    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+	import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 from Vector import Vector
 from Line import Line
 import random, sys
@@ -9,131 +9,109 @@ import random, sys
 sys.setrecursionlimit(10000)
 
 class Terrain:
-    def __init__(self, width, height):
-        self.walls = []
-        self.pathSize = 100
-        self.stack = [(self.pathSize,self.pathSize)]
-        self.visited = []
-        self.lines = []
-        self.width = width
-        self.height = height
-        self.pickupItems =[]
-        self.placeItemPickUP(5)
+	def __init__(self, width, height):
+		self.walls = []
+		self.pathSize = 100
+		self.stack = [(self.pathSize,self.pathSize)]
+		self.visited = []
+		self.lines = []
+		self.width = width
+		self.height = height
+		self.pickupItems =[]
 
 
-    def genRandomPoint(self):
-        x = Vector(random.randrange(1200),
-                        random.randrange(800))
-
-        for i in range(len(self.walls)):
-            if x == self.wall[i]:
-                return False
-
-        xYOfPickUp = x
-
-        return xYOfPickUp
+	def genRandomPoint(self):
+		listOfPoints = []
+		#Adds pick up to random coords not on a line
+		for i in range(0, 5):
+			point = Vector(random.randint(0, 1200), random.randint(0, 800))
+			if not(self.inWalls(point)):
+				listOfPoints.append(point)
+				
+		return listOfPoints
 
 
 
-    def placeItemPickUP(self, maxItems): # skeleton code for placing item pickups. NEEDS FIXING CURRENTLY LOOPS ROUND INFI
-        r = random
-        """xPosPickUp = r.randrange(1200)
-        yPosPickUp = r.randrange(800)
-        xYOfPickUp = Vector(random.randrange(1200),
-                        random.randrange(800))"""
-        itemRadius = 10
-        canPlaceItem = True
-        count = 0
-        for i in range(0, maxItems):# for each coordinate
-            xYOfPickUp = self.genRandomPoint()
-            for line in self.lines:
-                if line.distanceTo(xYOfPickUp) < itemRadius*2:
-                    canPlaceItem = False
-                else:
-                    canPlaceItem = True
-
-            if canPlaceItem == True:
-                self.pickupItems.append(xYOfPickUp)
-                print("canPlaceItem", canPlaceItem)
-                count = count + 1
-                print("count: ", count, ". MaxItems: ", maxItems)
-
-
-
-    def drawItemPickUp(self, canvas):
-        LINE_WIDTH = 2
-
-
-
-        for item in self.pickupItems:
-
-            canvas.draw_circle((item.x, item.y), 10, LINE_WIDTH, 'Red', 'Red')
-
-        return len(self.pickupItems)
-
-
-
-
-
-    def outFrame(self, x, y, width, height):
-        if x < 0+self.pathSize or y < 0+self.pathSize or x > width-self.pathSize or y > height-self.pathSize:
-            return True
-
-    def inVisited(self, x, y):
-        for i in range(len(self.visited)):
-            if (x, y) == (self.visited[i][0], self.visited[i][1]):
-                return True
-
-    def inStack(self, x, y):
-        for i in range(len(self.stack)):
-            if (x, y) == (self.stack[i][0], self.stack[i][1]):
-                return True
-
-    def valid(self, aList, width, height):
-        newList = []
-        for i in range(len(aList)):
-            if self.outFrame(aList[i][0], aList[i][1], width, height):
-                continue
-            if self.inVisited(aList[i][0], aList[i][1]):
-                continue
-            if self.inStack(aList[i][0], aList[i][1]):
-                continue
-            newList.append((aList[i][0], aList[i][1]))	
-        return newList
-
-    def genMaze(self, width, height, x, y):
-        while self.stack:
-            pointer = self.stack[-1]
-            self.walls.append(Vector(pointer[0], pointer[1]))
-            #Create list of valid negibours
-            neigh = [(pointer[0]+self.pathSize, pointer[1]),
-                    (pointer[0]-self.pathSize, pointer[1]),
-                    (pointer[0], pointer[1]+self.pathSize),
-                    (pointer[0], pointer[1]-self.pathSize)]
-            validNeigh = self.valid(neigh, width, height)
-            random.shuffle(validNeigh)
-            if(len(validNeigh)==0):
-                self.visited.append((pointer[0],pointer[1]))
-                del self.stack[-1]
-            else:
-                self.stack.append((validNeigh[0][0], validNeigh[0][1]))
-        self.getLines()
-
-
-    def getWalls(self):
-        return self.walls
-
-    def getLines(self):
-        for i in range(len(self.walls)-1):
-            self.lines.append(Line(Vector(self.walls[i].x, self.walls[i].y), Vector(self.walls[i+1].x,
-                self.walls[i+1].y)))
-        self.lines.append(Line(Vector(3,3), Vector(3, self.height-3)))
-        self.lines.append(Line(Vector(3,self.height-3), Vector(self.width-3, self.height-3)))
-        self.lines.append(Line(Vector(self.width-3, self.height-3), Vector(self.width-3, 3)))
-        self.lines.append(Line(Vector(self.width-3, 3), Vector(3, 3)))
-
-        print(self.lines)
 	
-    def drawWalls(self, canvas):
-        for line in self.lines:
-            line.draw(canvas)
+	def drawItemPickUp(self, canvas):
+		LINE_WIDTH = 2
+
+		self.pickupItems = self.genRandomPoint()
+		for item in self.pickupItems:
+			canvas.draw_circle(item.getP(), 10, LINE_WIDTH, 'Red', 'Red')
+
+
+
+
+
+
+	def outFrame(self, x, y, width, height):
+		if x < 0+self.pathSize or y < 0+self.pathSize or x > width-self.pathSize or y > height-self.pathSize:
+			return True
+
+	def inVisited(self, x, y):
+		for i in range(len(self.visited)):
+			if (x, y) == (self.visited[i][0], self.visited[i][1]):
+				return True
+	def inWalls(self, point):
+		for i in range(len(self.walls)):
+			if(point == self.walls[i]):
+				return True
+
+	def inStack(self, x, y):
+		for i in range(len(self.stack)):
+			if (x, y) == (self.stack[i][0], self.stack[i][1]):
+				return True
+
+	def valid(self, aList, width, height):
+		newList = []
+		for i in range(len(aList)):
+			if self.outFrame(aList[i][0], aList[i][1], width, height):
+				continue
+			if self.inVisited(aList[i][0], aList[i][1]):
+				continue
+			if self.inStack(aList[i][0], aList[i][1]):
+				continue
+			newList.append((aList[i][0], aList[i][1]))	
+		return newList
+
+	def genMaze(self, width, height, x, y):
+		while self.stack:
+			pointer = self.stack[-1]
+			self.walls.append(Vector(pointer[0], pointer[1]))
+			#Create list of valid negibours
+			neigh = [(pointer[0]+self.pathSize, pointer[1]),
+					(pointer[0]-self.pathSize, pointer[1]),
+					(pointer[0], pointer[1]+self.pathSize),
+					(pointer[0], pointer[1]-self.pathSize)]
+			validNeigh = self.valid(neigh, width, height)
+			random.shuffle(validNeigh)
+			if(len(validNeigh)==0):
+				self.visited.append((pointer[0],pointer[1]))
+				del self.stack[-1]
+			else:
+				self.stack.append((validNeigh[0][0], validNeigh[0][1]))
+		self.getLines()
+
+
+	def getWalls(self):
+		return self.walls
+
+	def getLines(self):
+		for i in range(len(self.walls)-1):
+			self.lines.append(Line(Vector(self.walls[i].x, self.walls[i].y), Vector(self.walls[i+1].x,
+				self.walls[i+1].y)))
+		self.lines.append(Line(Vector(3,3), Vector(3, self.height-3)))
+		self.lines.append(Line(Vector(3,self.height-3), Vector(self.width-3, self.height-3)))
+		self.lines.append(Line(Vector(self.width-3, self.height-3), Vector(self.width-3, 3)))
+		self.lines.append(Line(Vector(self.width-3, 3), Vector(3, 3)))
+
+	
+	def drawWalls(self, canvas):
+		for line in self.lines:
+			line.draw(canvas)
+
+t1 = Terrain(0,0)
+
+t1.genRandomPoint()
+
